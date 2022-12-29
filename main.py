5db4529291813e5b603e939acc2c7e17c1270582
Mark Szymanski <mark.szymanski@socotra.com>
2022-12-29 07:52:42 -0500

    financial txn demo for 0.0.3

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 from socotra_datamart_reports import OnRiskReport
 from socotra_datamart_reports import AllPoliciesReport
 from socotra_datamart_reports import TransactionFinancialImpactReport
-
+from socotra_datamart_reports import FinancialTransactionsReport
 
 load_dotenv()
 
@@ -16,13 +16,25 @@ if __name__ == "__main__":
           'database': os.environ.get('REPORT_DATABASE')
      }
 
+     start_timestamp = 1659326400000
+     end_timestamp = 1864596800000
+
      orr = OnRiskReport(creds)
-     orr.write_on_risk_report('personal-auto', 1667278800000, 'on_risk_report_1.csv')
+     orr.write_on_risk_report(
+          'personal-auto', start_timestamp,
+          f'on_risk_report_{start_timestamp}.csv')
 
      apr = AllPoliciesReport(creds)
-     apr.write_all_policies_report('personal-auto', 1659326400000, 1664596800000, 'all_policies_report_1.csv')
+     apr.write_all_policies_report(
+          'personal-auto', start_timestamp, end_timestamp,
+          f'all_policies_report_{start_timestamp}-{end_timestamp}.csv')
 
      tfir = TransactionFinancialImpactReport(creds)
      tfir.write_transaction_financial_impact_report(
-          'personal-auto', 0, 1864596800000, 'transaction_financial_impact_report_1.csv')
+          'personal-auto', start_timestamp, end_timestamp,
+          f'txn_financial_impact_report_{start_timestamp}-{end_timestamp}.csv')
 
+     ftr = FinancialTransactionsReport(creds)
+     ftr.write_financial_transactions_report(
+          start_timestamp, end_timestamp,
+          f'financial_txns_{start_timestamp}-{end_timestamp}.csv')
